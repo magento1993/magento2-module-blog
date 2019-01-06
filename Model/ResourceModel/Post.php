10e3200080d0b975eec6c62b0ce0f3632dfb0344
boris@acer <xinu@yandex.ru>
2019-01-06 22:04:15 +0300

    Initial version

---
+++ Model/ResourceModel/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Boangri\Blog\Model\ResourceModel;
+
+use \Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class Post extends AbstractDb
+{
+    /**
+     * Post Abstract Resource Constructor
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('boangri_blog_post', 'post_id');
+    }
+}
